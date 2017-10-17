da787538f1b944a8e04a0e4dff231683a8987987
Lismera <32731975+Lismera@users.noreply.github.com>
2017-10-17 11:16:59 +0100

    Update abitities.py

--- abitities.py
+++ abitities.py
@@ -8,5 +8,19 @@ ABILITIES = {
         "name": "Tactical Chunder",
         "damage": 15,
         "tier": 0
-    }
-}
+    },
+    "meeting_peers": {
+        "name": "Meeting Friend"
+        "damage": 10,
+        "tier": 1
+    },
+    "meeting_lecturer": {
+        "name": "Meeting Lecturer"
+        "damage": -15 #so scared that you try to pretend not to be drunk and restore some hp
+        "tier": 1
+    },
+    "shot_boost": {
+        "name": "Shot Boost"
+        "damage": 5
+        "tier": 2 #if you drink three shots in a raw you have a reduced damage
+},
