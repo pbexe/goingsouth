3db4cedd77735fc32a1e53624d86da95e8f34b69
Miles Budden <miles@budden.net>
2017-10-17 10:33:12 +0100

    Added attributes to player.py

--- player.py
+++ player.py
@@ -1,7 +1,14 @@
 from items import *
 from map import rooms
+from abilities import abilities
 
 inventory = [item_id, item_laptop, item_money]
 
 # Start game at the reception
 current_room = rooms["Reception"]
+
+money = 10
+
+health = 100
+
+player_abilities = []
