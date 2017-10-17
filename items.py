acc0452ee34336c22d894ad7254f2c4deb891fa3
Unknown <ben14367@hotmail.com>
2017-10-17 10:47:20 +0100

    added first items

--- items.py
+++ items.py
@@ -1,57 +1,24 @@
-item_id = {
-    "id": "id",
-
-    "name": "id card",
-
-    "description":
-    """You new shiny student ID card. Expires 1 June 2017.
-You wonder why they have printed a suicide hotline number on it?...""",
-    "mass": 0.2
-}
-
-item_laptop = {
-    "id": "laptop",
-
-    "name": "laptop",
-
-    "description":
-    "It has seen better days. At least it has a WiFi card!",
-    "mass": 1
-}
-
 item_money = {
     "id": "money",
 
     "name": "money",
 
-    "description":
-    "This wad of cash is barely enough to pay your tuition fees.",
-    "mass": 0.6
-}
+    "description": "a new crisp plastic note",
 
-item_biscuits = {
-    "id": "biscuits",
+    "is_alcohol": False,
 
-    "name": "a pack of biscuits",
-
-    "description": "A pack of biscuits.",
-    "mass": 1.3
-}
-
-item_pen = {
-    "id": "pen",
+    "is_substance": False
     
-    "name": "a pen",
-
-    "description": "A basic ballpoint pen.",
-    "mass": 0.2
 }
 
-item_handbook = {
-    "id": "handbook",
-    
-    "name": "a student handbook",
+item_id = {
+    "id": "id",
 
-    "description": "This student handbook explains everything. Seriously.",
-    "mass": 1.1
+    "name": "driving licence",
+
+    "description": "a driving licence that looks... almost like you, you need it to get into the club",
+
+    "is_alcohol": False,
+
+    "is_substance": False
 }
