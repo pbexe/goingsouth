415a3788a03bd9cf4f2954890666c5b5b44a7572
Miles Budden <miles@budden.net>
2017-10-17 10:31:46 +0100

    Added the base for the abilities file

---
+++ abitities.py
@@ -0,0 +1,12 @@
+ABILITIES = {
+    "drunken_insult": {
+        "name": "Drunken Insult",
+        "damage": 10,
+        "tier": 0
+    },
+    "tac_chunder": {
+        "name": "Tactical Chunder",
+        "damage": 15,
+        "tier": 0
+    }
+}
