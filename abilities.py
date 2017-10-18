eb3b8bed9eb3fa6a66694d8883a5311b3b389bcd
Miles Budden <miles@budden.net>
2017-10-18 21:18:39 +0100

    Renamed abilities.py to remove typo

---
+++ abilities.py
@@ -0,0 +1,42 @@
+ABILITIES = {
+    "drunken_insult": {
+        "name": "Drunken Insult",
+        "damage": 10,
+        "tier": 0
+    },
+    "tac_chunder": {
+        "name": "Tacticaly Chunder On Them",
+        "damage": 15,
+        "tier": 0
+    },
+    "bottle": {
+        "name": "Bottle Them",
+        "damage": 30,
+        "tier": 1
+    },
+    "steal": {
+        "name": "Steal Yo Girl",
+        "damage": 99,
+        "tier": 3
+    },
+    "hadouken": {
+        "name": "Hadouken",
+        "damage": 30,
+        "tier": 1
+    },
+    "dance": {
+        "name": "Show them up in a dance off",
+        "damage": 40,
+        "tier": 2
+    },
+    "blackmail": {
+        "name": "Blackmail them",
+        "damage": 30,
+        "tier": 1
+    },
+    "spike": {
+        "name": "Spike their drink",
+        "damage": 50,
+        "tier": 2
+    },
+}
